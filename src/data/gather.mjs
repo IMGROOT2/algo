2b6bfa2a42dcf2794274b45a8ccc4cf07859994d
Ruhan Gupta <ruhan_gupta@outlook.com>
2023-04-03 00:18:18 -0500

    Beta Release, Fixed Bugs

--- src/data/gather.mjs
+++ src/data/gather.mjs
@@ -1,51 +1,54 @@
 import fs from 'fs';
 
 (async () => {
-    for(let i = 1; i < 2000; i++) { // 2000 is a rough max of CPIDs
+    for(let i = 1; i <= 2000; i++) { // 2000 is a rough max of CPIDs
         try {
-            console.log("trying " + i);
+            console.log("Trying " + i + "...");
             const response = await fetch('http://usaco.org/index.php?page=viewproblem2&cpid=' + i);
             const body = await response.text();
             const title = body.match(/<h2>(.*?)<\/h2>/)[1];
             const difficulty = title.match(/Bronze|Silver|Gold|Platinum/)[0];
+            const year = title.match(/20\d\d/)[0];
+            if(year > 2015) {
+                if (difficulty === 'Bronze') {
+                    console.log('Found problem ' + i + ' with difficulty Bronze and year ' + year + '.');
+                    const bronze = JSON.parse(fs.readFileSync('bronze.json', 'utf8'));
+                    if (!bronze.includes(i)) {
+                        bronze.push(i);
+                        fs.writeFileSync('bronze.json', JSON.stringify(bronze));
+                    }
+                } else if (difficulty === 'Silver') {
+                    console.log('Found problem ' + i + ' with difficulty Silver and year ' + year + '.');
+                    const silver = JSON.parse(fs.readFileSync('silver.json', 'utf8'));
+                    if (!silver.includes(i)) {
+                        silver.push(i);
+                        fs.writeFileSync('silver.json', JSON.stringify(silver));
+                    }
+                } else if (difficulty === 'Gold') {
+                    console.log('Found problem ' + i + ' with difficulty Gold and year ' + year + '.');
+                    const gold = JSON.parse(fs.readFileSync('gold.json', 'utf8'));
+                    if (!gold.includes(i)) {
+                        gold.push(i);
+                        fs.writeFileSync('gold.json', JSON.stringify(gold));
+                    }
 
-            if(difficulty === 'Bronze') {
-                console.log("bronze");
-                const bronze = JSON.parse(fs.readFileSync('bronze.json', 'utf8'));
-                if(!bronze.includes(i)) {
-                    bronze.push(i);
-                    fs.writeFileSync('bronze.json', JSON.stringify(bronze));
+                } else if (difficulty === 'Platinum') {
+                    console.log('Found problem ' + i + ' with difficulty Platinum and year ' + year + '.');
+                    const platinum = JSON.parse(fs.readFileSync('platinum.json', 'utf8'));
+                    if (!platinum.includes(i)) {
+                        platinum.push(i);
+                        fs.writeFileSync('platinum.json', JSON.stringify(platinum));
+                    }
+
                 }
             }
-            else if(difficulty === 'Silver') {
-                console.log("silver");
-                const silver = JSON.parse(fs.readFileSync('silver.json', 'utf8'));
-                if(!silver.includes(i)) {
-                    silver.push(i);
-                    fs.writeFileSync('silver.json', JSON.stringify(silver));
-                }
-            }
-            else if(difficulty === 'Gold') {
-                console.log("gold");
-                const gold = JSON.parse(fs.readFileSync('gold.json', 'utf8'));
-                if(!gold.includes(i)) {
-                    gold.push(i);
-                    fs.writeFileSync('gold.json', JSON.stringify(gold));
-                }
-
-            }
-            else if(difficulty === 'Platinum') {
-                console.log("platinum");
-                const platinum = JSON.parse(fs.readFileSync('platinum.json', 'utf8'));
-                if(!platinum.includes(i)) {
-                    platinum.push(i);
-                    fs.writeFileSync('platinum.json', JSON.stringify(platinum));
-                }
-
+            else {
+                console.log('ID ' + i + ' is too old (' + year + ').')
             }
         }
         catch(e) {
-            console.log("no problem with id " + i + " found");
+            console.log("ID " + i + " doesn't exist.");
         }
     }
+    console.log("Done!");
 })();
--- src/js/solve.js
+++ src/js/solve.js
@@ -35,6 +35,43 @@ auth.onAuthStateChanged(async user => {
                 }
             }
         }
+        try {
+            options.generate.classList.toggle("is-loading");
+            problem.problem.classList.toggle("is-hidden");
+            problem.loader.classList.toggle("is-hidden");
+            if(localStorage.getItem("problem") === null) {
+                bulmaToast.toast({ message: "Oops! Couldn't load previous problem.", type: 'is-warning' });
+                options.generate.classList.toggle("is-loading");
+                problem.problem.classList.toggle("is-hidden");
+                problem.loader.classList.toggle("is-hidden");
+            }
+            else {
+                const result = await generateProblem("custom-id", localStorage.getItem("problem"));
+                console.log("dida");
+                console.log("result: " + result);
+                if(result === "error") {
+                    toast({ message: "Oops! Couldn't load previous problem.", type: 'is-warning' });
+                    console.error("Issue with generateProblem().");
+                    options.generate.classList.toggle("is-loading");
+                    problem.problem.classList.toggle("is-hidden");
+                    problem.loader.classList.toggle("is-hidden");
+                }
+                else if(result === "success"){
+                    options.generate.classList.toggle("is-loading");
+                    problem.problem.classList.toggle("is-hidden");
+                    problem.loader.classList.toggle("is-hidden");
+                    problem.link.classList.remove("is-hidden");
+                    bulmaToast.toast({message: 'Restored previous problem!', type: 'is-success'});
+                }
+            }
+        }
+        catch(err) {
+            toast({ message: "Oops! Couldn't load previous problem.", type: 'is-warning' });
+            console.error(err.message);
+            options.generate.classList.toggle("is-loading");
+            problem.problem.classList.toggle("is-hidden");
+            problem.loader.classList.toggle("is-hidden");
+        }
         document.getElementById("solve-button").classList.add("is-hidden");
     }
     else {
@@ -51,72 +88,110 @@ for(let i = 0; i < options.items.length; i++) {
         options.items[i].classList.add("is-active");
     });
 }
-options.generate.addEventListener("click", () => {
+options.generate.addEventListener("click", async () => {
     options.generate.classList.toggle("is-loading");
     problem.problem.classList.toggle("is-hidden");
     problem.loader.classList.toggle("is-hidden");
     try {
-        const difficulty = options.label.innerText.toLowerCase();
-        let id = -1;
-        if(difficulty === "bronze") {
-            // pick a random number from the data in bronze.json
-            const bronze = JSON.parse(fs.readFileSync('./src/data/bronze.json', 'utf8'));
-            id = bronze[Math.floor(Math.random() * bronze.length)];
-        }
-        else if(difficulty === "silver") {
-            // pick a random number from the data in silver.json
-            const silver = JSON.parse(fs.readFileSync('./src/data/silver.json', 'utf8'));
-            id = silver[Math.floor(Math.random() * silver.length)];
-        }
-        else if(difficulty === "gold") {
-            // pick a random number from the data in gold.json
-            const gold = JSON.parse(fs.readFileSync('./src/data/gold.json', 'utf8'));
-            id = gold[Math.floor(Math.random() * gold.length)];
-        }
-        else if(difficulty === "platinum") {
-            // pick a random number from the data in platinum.json
-            const platinum = JSON.parse(fs.readFileSync('./src/data/platinum.json', 'utf8'));
-            id = platinum[Math.floor(Math.random() * platinum.length)];
-        }
-
-
-
-        fetch('https://proxy.cors.sh/http://usaco.org/index.php?page=viewproblem2&cpid=' + id, {
-            headers: {
-                'x-cors-api-key': 'temp_7919b51de298c6f3f593eaab8a3c2160'
+        if (localStorage.getItem("options") === null) {
+            bulmaToast.toast({
+                message: "Oops! You haven't selected a difficulty. Select a difficulty and try again.",
+                type: 'is-warning'
+            });
+            options.generate.classList.toggle("is-loading");
+            problem.problem.classList.toggle("is-hidden");
+            problem.loader.classList.toggle("is-hidden");
+        } else {
+            const result = await generateProblem("random-id", localStorage.getItem("options").toLowerCase());
+            if (result === "error") {
+                toast({message: 'Error generating problem. Please try again later.', type: 'is-danger'});
+                console.error("Issue with generateProblem().");
+                options.generate.classList.toggle("is-loading");
+                problem.problem.classList.toggle("is-hidden");
+                problem.loader.classList.toggle("is-hidden");
+            } else if (result === "success") {
+                options.generate.classList.toggle("is-loading");
+                problem.problem.classList.toggle("is-hidden");
+                problem.loader.classList.toggle("is-hidden");
+                problem.link.classList.remove("is-hidden");
+                bulmaToast.toast({message: 'New problem generated!', type: 'is-success'});
             }
-        })
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const html = parser.parseFromString(data, 'text/html');
-            problem.title.innerText = html.getElementsByClassName("panel")[0].getElementsByTagName("h2")[1].innerText;
-            problem.subtitle.innerText = html.getElementsByClassName("panel")[0].getElementsByTagName("h2")[0].innerText;
-            problem.text.innerHTML = html.getElementById("probtext-text").innerHTML;
-            problem.link.href = 'http://usaco.org/index.php?page=viewproblem2&cpid=' + id;
-        }).then(() => {
-            window.renderMathInElement(document.getElementById("problem-text"), {delimiters: [
-                    {left: '$$', right: '$$', display: true},
-                    {left: '$', right: '$', display: false},
-                    {left: '\\(', right: '\\)', display: false},
-                    {left: '\\[', right: '\\]', display: true}
-                ]});
-        });
-
-
-
-        options.generate.classList.toggle("is-loading");
-        problem.problem.classList.toggle("is-hidden");
-        problem.loader.classList.toggle("is-hidden");
-        problem.link.classList.remove("is-hidden");
-        bulmaToast.toast({ message: 'New problem generated!', type: 'is-success' });
-
-    }
-    catch(err) {
-        toast({ message: 'Error generating problem. Please try again later.', type: 'is-danger' });
+        }
+    } catch (err) {
+        toast({message: 'Error generating problem. Please try again later.', type: 'is-danger'});
         console.error(err.message);
         options.generate.classList.toggle("is-loading");
         problem.problem.classList.toggle("is-hidden");
         problem.loader.classList.toggle("is-hidden");
     }
 });
+async function generateProblem(idType, data) {
+    return new Promise(resolve => {
+
+        try {
+            var id = -1;
+            if(idType === "random-id") {
+                if (data === "bronze") {
+                    // pick a random number from the data in bronze.json
+                    const bronze = JSON.parse(fs.readFileSync('./src/data/bronze.json', 'utf8'));
+                    id = bronze[Math.floor(Math.random() * bronze.length)];
+                } else if (data === "silver") {
+                    // pick a random number from the data in silver.json
+                    const silver = JSON.parse(fs.readFileSync('./src/data/silver.json', 'utf8'));
+                    id = silver[Math.floor(Math.random() * silver.length)];
+                } else if (data === "gold") {
+                    // pick a random number from the data in gold.json
+                    const gold = JSON.parse(fs.readFileSync('./src/data/gold.json', 'utf8'));
+                    id = gold[Math.floor(Math.random() * gold.length)];
+                } else if (data === "platinum") {
+                    // pick a random number from the data in platinum.json
+                    const platinum = JSON.parse(fs.readFileSync('./src/data/platinum.json', 'utf8'));
+                    id = platinum[Math.floor(Math.random() * platinum.length)];
+                }
+            }
+            else if(idType === "custom-id") {
+                id = data;
+            }
+
+
+            fetch('https://proxy.cors.sh/http://usaco.org/index.php?page=viewproblem2&cpid=' + id, {
+                headers: {
+                    'x-cors-api-key': 'temp_f4ed01f6394b2325ead5f8bad55f4e65'
+                }
+            })
+                .then(response => response.text())
+                .then(data => {
+                    try {
+                        const parser = new DOMParser();
+                        const html = parser.parseFromString(data, 'text/html');
+                        problem.title.innerText = html.getElementsByClassName("panel")[0].getElementsByTagName("h2")[1].innerText;
+                        problem.subtitle.innerText = html.getElementsByClassName("panel")[0].getElementsByTagName("h2")[0].innerText;
+                        problem.text.innerHTML = html.getElementById("probtext-text").innerHTML;
+                        problem.link.href = 'http://usaco.org/index.php?page=viewproblem2&cpid=' + id;
+                    }
+                    catch(err) {
+                        console.error(err.message);
+                        console.log("oops");
+                        resolve("error");
+                    }
+                }).then(() => {
+                window.renderMathInElement(document.getElementById("problem-text"), {
+                    delimiters: [
+                        {left: '$$', right: '$$', display: true},
+                        {left: '$', right: '$', display: false},
+                        {left: '\\(', right: '\\)', display: false},
+                        {left: '\\[', right: '\\]', display: true}
+                    ]
+                });
+                localStorage.setItem("problem", id);
+                console.log("did");
+                resolve("success");
+            });
+        }
+        catch(err) {
+            console.error(err.message);
+            console.log("oops");
+            resolve("error");
+        }
+    });
+}
