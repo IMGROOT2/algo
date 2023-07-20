14c209e79541ea69f39fba9353eaafc9221dcc3a
Ruhan Gupta <ruhan_gupta@outlook.com>
2023-07-20 17:46:02 -0500

    Update stats, fix bug

---
+++ src/js/home.js
@@ -0,0 +1,13 @@
+// Note to viewers: I'm currently updating these stats manually- I'll implement the API soon ;)
+const users = "730";
+const views = "5,500";
+const countries = "40";
+
+
+const usersSpan = document.getElementById("users");
+const viewsSpan = document.getElementById("views");
+const countriesSpan = document.getElementById("countries");
+
+usersSpan.innerText = users + "+";
+viewsSpan.innerText = views + "+";
+countriesSpan.innerText = countries + "+";
